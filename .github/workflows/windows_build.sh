07bf0bd3412c1aa83074e89c785f3635bf505935
Martin Hickson <martin.hickson@gmail.com>
2026-08-03 04:51:40 +1200

    Supply Pack200 jar to release builds
    ResourceDownloader compiles against io.pack200, so configure must
    receive pack.jar via --with-pack. Download the same verified artifact
    used by 1.8.8.21 on Linux, Portable, macOS, and Windows.

--- .github/workflows/windows_build.sh
+++ .github/workflows/windows_build.sh
@@ -4,12 +4,13 @@ export RUSTFLAGS="-C target-feature=+crt-static"
 export ICEDTEAWEB_INSTALL="$(cygpath -u "${WORKSPACE}/icedtea-web-image")"
 export WIXPATH="$(cygpath -u "C:/PROGRA~2/WIXTOO~1.14/bin")"
 export WIXGEN="$(cygpath -u "C:/cygwin64/usr/share/java/wixgen.jar")"
+export PACK_JAR="$(cygpath -u "C:/cygwin64/usr/share/java/pack.jar")"
 export PATH="${PATH}:/cygdrive/c/rust/bin:${WIXPATH}"
 export JVM_HOME_SHORT="$(cygpath -d "${JAVA_HOME}")"
 export JVMPATH="$(cygpath -u ${JVM_HOME_SHORT})"
 echo "Configure IcedTea-Web"
 ./autogen.sh
-./configure --disable-native-plugin --prefix="${ICEDTEAWEB_INSTALL}" --with-wix=${WIXPATH} --with-wixgen=${WIXGEN} --with-itw-libs=BUNDLED --with-jdk-home="${JVMPATH}"
+./configure --disable-native-plugin --prefix="${ICEDTEAWEB_INSTALL}" --with-wix=${WIXPATH} --with-wixgen=${WIXGEN} --with-itw-libs=BUNDLED --with-pack="${PACK_JAR}" --with-jdk-home="${JVMPATH}"
 echo "Build IcedTea-Web"
 make
 echo "Create IcedTea-Web Distribution"
@@ -19,6 +20,7 @@ echo "Create IcedTea-Web MSI"
 make win-installer
 find . -name "icedtea-web-*.msi"
 echo "Generate shasums"
+shopt -s nullglob
 for zip in icedtea-web-*.win.bin.zip; do
 	shasum -a 256 "$zip" > "$zip.sha256.txt"
 done
